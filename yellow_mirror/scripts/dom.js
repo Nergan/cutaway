6ee920109b5569b5fbc1f894ff6bb7b0c540752d
Nergan <mak.kriuchkov@yandex.ru>
2026-03-06 14:47:22 +0700

    try

--- yellow_mirror/scripts/dom.js
+++ yellow_mirror/scripts/dom.js
@@ -5,8 +5,6 @@ YM.elements = {
     minimizedBar: document.getElementById('minimizedBar'),
     input: document.getElementById('url-input'),
     button: document.getElementById('load-site-btn'),
-    streamContainer: document.getElementById('stream-container'),
-    streamImage: document.getElementById('stream-image'),
-    clickOverlay: document.getElementById('click-overlay'),
-    backgroundVideo: document.getElementById('background-video')
+    iframe: document.getElementById('site-frame')
+    // splash удалён
 };
