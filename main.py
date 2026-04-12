14ea803efb278babf18fcc3e017cc6e9db677219
Nergan <mak.kriuchkov@yandex.ru>
2026-04-12 20:26:43 +0700

    fix

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ from snake.snake import router as snake_router
 from toadbin.toadbin import router as toadbin_router
 from formular.formular import router as formular_router
 from yellow_mirror.yellow_mirror import router as yellow_mirror_router, shutdown_clients
-# from simple_aichat.simple_aichat import router as simple_aichat_router
+from simple_aichat.simple_aichat import router as simple_aichat_router
 
 load_dotenv()
 
@@ -53,7 +53,7 @@ app.include_router(snake_router, prefix='/snake')
 app.include_router(toadbin_router, prefix='/toadbin')
 app.include_router(formular_router, prefix='/formular')
 app.include_router(yellow_mirror_router, prefix='/yellow-mirror')
-# app.include_router(simple_aichat_router, prefix='/simple-aichat')
+app.include_router(simple_aichat_router, prefix='/simple-aichat')
 
 
 class TrackRequest(BaseModel):
