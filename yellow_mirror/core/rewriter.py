cd5d31e2cb3d053ea2e180a956f140c90c8cfbc7
Nergan <mak.kriuchkov@yandex.ru>
2026-05-08 14:49:44 +0700

    Well, a server is needed..

--- yellow_mirror/core/rewriter.py
+++ yellow_mirror/core/rewriter.py
@@ -1,3 +1,4 @@
+import re
 from bs4 import BeautifulSoup
 
 def rewrite_html(html: str, target_url: str) -> str:
@@ -10,19 +11,35 @@ def rewrite_html(html: str, target_url: str) -> str:
         else:
             soup.append(head)
 
-    # Resolves relative links implicitly
+    # Base tag for relative links
     base_tag = soup.new_tag("base", href=f"/yellow-mirror/proxy/{target_url}")
     soup.head.insert(0, base_tag)
     
-    # Hook for browsing history sync
+    # Injector script
     script_tag = soup.new_tag("script", src="/yellow_mirror/scripts/inject.js")
     soup.head.insert(1, script_tag)
     
-    # Rewrite absolute resource links
+    # 1. KILL SUBRESOURCE INTEGRITY (Fixes Twitch modules)
+    for tag in soup.find_all(True):
+        if 'integrity' in tag.attrs:
+            del tag['integrity']
+        if 'nonce' in tag.attrs:
+            del tag['nonce']
+            
+    # 2. FIX IMAGES (Forces fallback to standard src)
+    for tag in soup.find_all(['img', 'source']):
+        if tag.has_attr('srcset'):
+            del tag['srcset']
+
+    # 3. DESTROY CSP META TAGS
+    for meta in soup.find_all('meta', attrs={'http-equiv': lambda x: x and x.lower() == 'content-security-policy'}):
+        meta.decompose()
+    
+    # Rewrite absolute links
     for tag, attr in[('a', 'href'), ('link', 'href'), ('script', 'src'), ('img', 'src'), ('iframe', 'src'), ('form', 'action')]:
         for el in soup.find_all(tag, **{attr: True}):
             val = el.get(attr, "")
-            if val.startswith("http://") or val.startswith("https://"):
+            if val.startswith(("http://", "https://")):
                 el[attr] = f"/yellow-mirror/proxy/{val}"
                 
     return str(soup)
