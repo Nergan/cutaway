8f0e04743db7ece5fb6a50d00826e560bc1b97da
Nergan <mak.kriuchkov@yandex.ru>
2026-07-28 19:59:41 +0700

    upd

--- netlazy/frontend/src/components/Feed.vue
+++ netlazy/frontend/src/components/Feed.vue
@@ -8,7 +8,7 @@
         </transition>
         
         <transition name="dropdown-fade">
-          <div class="glass-menu" v-if="filterText && visibleSearchTags.length > 0" :style="{ top: isMobile ? 'auto' : '100%', bottom: isMobile ? '100%' : 'auto', left: '0', right: '0', maxHeight: '250px', width: '100%' }">
+          <div class="glass-menu" v-if="filterText && visibleSearchTags.length > 0" :style="{ top: isMobile ? 'auto' : '100%', bottom: isMobile ? '100%' : 'auto', left: '0', right: '0', maxHeight: '250px', overflowY: 'auto', width: '100%' }">
             <transition-group name="tag-list" tag="div">
               <div class="glass-option" v-for="(tag, idx) in visibleSearchTags.slice(0, 15)" :key="'ac-'+tag.name" :class="{'highlighted-option': idx === highlightIndex}" @mousedown="animateAndSelectTag($event, tag)">
                 <span class="animated-underline">{{ store.getLocalizedTag(tag.name) }}</span>
